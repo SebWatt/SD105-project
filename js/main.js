50ebed7eea6f9663c70eaabbbc6e468606e0718b
Sebastien Watt <sebastienwatt@icloud.com>
2023-03-02 13:27:10 -0800

    Made about and skills sections with styling for mobile devices

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 const sideMenu = document.getElementsByClassName("side-menu")[0];
 const sideMenuIcon = document.querySelector(".side-menu i");
+const sideMenuItems = document.querySelectorAll(".side-menu li");
+
 function openHamburgerMenuBtn() {
   sideMenu.style.width = "40%";
   sideMenu.style.zIndex = 1;
+  for (i = 0; i < sideMenuItems.length; i++) {
+    sideMenuItems[i].style.display = "block";
+  }
 }
 function closeHamburgerMenuBtn() {
   sideMenu.style.width = "0%";
   sideMenu.style.zIndex = -1;
+  for (i = 0; i < sideMenuItems.length; i++) {
+    sideMenuItems[i].style.display = "none";
+  }
 }
-
 if (window.onresize >= "762px") {
   openHamburgerMenuBtn();
 }
